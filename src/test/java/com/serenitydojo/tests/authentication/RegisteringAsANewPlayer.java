091abe827b4d91e91e570744e834ec6bd8fa0db4
John Ferguson Smart <john.smart@wakaleo.com>
2024-02-17 15:20:08 +0000

    Added logic to enter country

--- src/test/java/com/serenitydojo/tests/authentication/RegisteringAsANewPlayer.java
+++ src/test/java/com/serenitydojo/tests/authentication/RegisteringAsANewPlayer.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -48,6 +49,7 @@ public class RegisteringAsANewPlayer {
         driver.findElement(By.id("name")).sendKeys(player.name());
         driver.findElement(By.id("email")).sendKeys(player.email());
         driver.findElement(By.id("password")).sendKeys(player.password());
+        new Select(driver.findElement(By.id("country"))).selectByVisibleText("United Kingdom");
         driver.findElement(By.id("create-account")).click();
 
         // Now try to login
