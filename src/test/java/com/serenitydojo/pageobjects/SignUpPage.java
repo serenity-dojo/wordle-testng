27d11504ae69c34364c6fb7e866810a8f87b14a1
John Ferguson Smart <john.smart@wakaleo.com>
2024-02-17 15:14:16 +0000

    Added logic to enter country

--- src/test/java/com/serenitydojo/pageobjects/SignUpPage.java
+++ src/test/java/com/serenitydojo/pageobjects/SignUpPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,6 +14,7 @@ public class SignUpPage extends WordlePageComponent {
     private static final By NAME_FIELD = By.id("name");
     private static final By EMAIL_FIELD = By.id("email");
     private static final By PASSWORD_FIELD = By.id("password");
+    private static final By COUNTRY_DROPDOWN = By.id("country");
     private static final By CREATE_ACCOUNT_BUTTON = By.id("create-account");
     private static final By ERROR_MESSAGES = By.cssSelector("[class*=text-red]");
 
@@ -25,6 +27,7 @@ public class SignUpPage extends WordlePageComponent {
         driver.findElement(NAME_FIELD).sendKeys(player.name());
         driver.findElement(EMAIL_FIELD).sendKeys(player.email());
         driver.findElement(PASSWORD_FIELD).sendKeys(player.password());
+        new Select(driver.findElement(COUNTRY_DROPDOWN)).selectByVisibleText("United Kingdom");
         driver.findElement(CREATE_ACCOUNT_BUTTON).click();
     }
 
